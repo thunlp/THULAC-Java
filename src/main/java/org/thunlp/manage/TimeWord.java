f56df6750a765e6ca84f48aa93dfe5f82c30a014
std4453 <std4453@foxmail.com>
2017-02-20 00:46:48 +0800

    Cleanup TimeWord.java

--- src/main/java/org/thunlp/manage/TimeWord.java
+++ src/main/java/org/thunlp/manage/TimeWord.java
@@ -1,269 +1,102 @@
 package org.thunlp.manage;
 
 import org.thunlp.base.TaggedSentence;
-
-import java.util.HashSet;
+import org.thunlp.base.WordWithTag;
+import org.thunlp.util.StringHelper;
 
 public class TimeWord {
-	HashSet<Integer> arabicNumSet;
-	//std::set<int> chineseNumSet;
-	HashSet<Integer> timeWordSet;
-	HashSet<Integer> otherSet;
+	private static final String ARABIC_NUMBER_CODE_POINTS =
+			StringHelper.toString(48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+					65296, 65297, 65298, 65299, 65300, 65301, 65302, 65303, 65304, 65305);
+	private static final String TIME_WORD_CODE_POINTS =
+			StringHelper.toString(24180, 26376, 26085, 21495, 26102, 28857, 20998, 31186);
+	private static final String OTHER_CODE_POINTS =
+			StringHelper.toString(65292, 12290, 65311, 65281, 65306, 65307, 8216, 8217,
+					8220, 8221, 12304, 12305, 12289, 12298, 12299, 126, 183, 64, 124, 35,
+					65509, 37, 8230, 38, 42, 65288, 65289, 8212, 45, 43, 61, 44, 46, 60,
+					62, 63, 47, 33, 59, 58, 39, 34, 123, 125, 91, 93, 92, 124, 35, 36, 37,
+					94, 38, 42, 40, 41, 95, 45, 43, 61, 9700, 9734, 9733, 65, 66, 67,
+					68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+					85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105,
+					106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+					120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57);
 
-	public TimeWord() {
-		this.arabicNumSet = new HashSet<>();
-		this.timeWordSet = new HashSet<>();
-		this.otherSet = new HashSet<>();
-
-		for (int i = 48; i < 58; i++) {
-			this.arabicNumSet.add(i);
-		}
-		for (int i = 65296; i < 65306; i++) {
-			this.arabicNumSet.add(i);
-		}
-			/*
-			int chineseNums[] = {12295,19968,20108,19977,22235,20116,20845,19971,20843,20061}; 
-			for(int i = 0; i < 10; i ++){
-				chineseNumSet.insert(chineseNums[i]);
-			}
-			*/
-
-		//��:24180 ��:26376 ��:26085 ��:21495 ʱ:26102 ��:28857 ��:20998 ��:31186
-		int timeWord[] = {24180, 26376, 26085, 21495, 26102, 28857, 20998, 31186};
-		int len = 8;
-		for (int i = 0; i < len; i++) {
-			this.timeWordSet.add(timeWord[i]);
-		}
-
-		for (int i = 65; i < 91; i++) {
-			this.otherSet.add(i);
-		}
-		for (int i = 97; i < 123; i++) {
-			this.otherSet.add(i);
-		}
-		for (int i = 48; i < 58; i++) {
-			this.otherSet.add(i);
-		}
-
-		int other[] = {65292, 12290, 65311, 65281, 65306, 65307, 8216, 8217, 8220, 8221, 12304, 12305,
-				12289, 12298, 12299, 126, 183, 64, 124, 35, 65509, 37, 8230, 38, 42, 65288,
-				65289, 8212, 45, 43, 61, 44, 46, 60, 62, 63, 47, 33, 59, 58, 39, 34, 123, 125,
-				91, 93, 92, 124, 35, 36, 37, 94, 38, 42, 40, 41, 95, 45, 43, 61, 9700, 9734, 9733};
-		len = 63;
-		for (int i = 0; i < len; i++) {
-			this.otherSet.add(other[i]);
-		}
-	}
-
-	public boolean isArabicNum(String word) {
-		boolean allArabic = true;
-		for (int i = 0; i < word.length(); i++) {
-			if (!this.arabicNumSet.contains(word.charAt(i))) {
-				allArabic = false;
-				break;
-			}
-		}
-		return allArabic;
-	}
-
-		/*
-		inline bool isChineseNum(Word word){
-			bool allChineseNum = true;
-			std::set<int>::iterator it;
-			for(int i = 0; i < word.size(); i ++){
-				it = chineseNumSet.find(word[i]);
-				if(it == chineseNumSet.end()){
-					allChineseNum = false;
-					break;
-				}
-			}
-			return allChineseNum;
-		}
-		*/
-
-	public boolean isTimeWord(String word) {
-		if (word.length() == 0 || word.length() > 1) {
-			return false;
-		}
-		if (!this.timeWordSet.contains((int) word.charAt(0))) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	public boolean isDoubleWord(String word, String postWord) {
-		if (word.length() != 1 || postWord.length() != 1) {
-			return false;
-		} else {
-			int wordInt = word.charAt(0);
-			int postWordInt = postWord.charAt(0);
-			if (wordInt == postWordInt) {
-				if (!this.otherSet.contains((int) wordInt)) {
-					return true;
-				} else {
-					return false;
-				}
-			} else {
+	private boolean isArabicNum(String word) {
+		int len = word.codePointCount(0, word.length());
+		for (int i = 0; i < len; i++)
+			if (ARABIC_NUMBER_CODE_POINTS.indexOf(word.codePointAt(i)) != -1)
 				return false;
-			}
+		return true;
+	}
+
+	private boolean isTimeWord(String word) {
+		return !(word.length() == 0 || word.length() > 1) &&
+				TIME_WORD_CODE_POINTS.indexOf(word.charAt(0)) != -1;
+	}
+
+	private boolean isDoubleWord(String word, String postWord) {
+		if (word.length() != 1 || postWord.length() != 1) return false;
+		else {
+			int wordInt = word.codePointAt(0);
+			int postWordInt = postWord.codePointAt(0);
+			return wordInt == postWordInt && OTHER_CODE_POINTS.indexOf(wordInt) != -1;
 		}
 	}
 
-	boolean isHttpWord(String word) {
-		if (word.length() < 5) {
-			return false;
-		} else {
-			if (word.charAt(0) == 'h' && word.charAt(1) == 't' && word.charAt(
-					2) == 't' && word.charAt(3) == 'p') {
-				return true;
-			} else {
-				return false;
-			}
-		}
+	private boolean isHttpWord(String word) {
+		return word.length() >= 5 && word.startsWith("http");
 	}
-
-//		public void adjust(SegmentedSentence sentence){
-//			int size = sentence.size();
-//			String word;
-//			int count = 0;
-//			boolean hasTimeWord = false;
-//
-//			for(int i = size - 1; i >= 0; i --){
-//				word = sentence.get(i);
-//				if(isTimeWord(word)){
-//					hasTimeWord = true;
-//				}else{
-//					if(hasTimeWord){
-//						//if(isArabicNum(word) || isChineseNum(word)){
-//						if(isArabicNum(word)){
-//							String stmp = sentence.get(i) + sentence.get(i+1);
-//	                        sentence.set(i,stmp);
-//							sentence.remove(i+1);
-//						}
-//						else {
-//							hasTimeWord = false;
-//						}
-//					}
-//				}
-//			}
-//
-//			size = sentence.size();
-//			String postWord;
-//			for(int i = size - 2; i >= 0; i --){
-//				word = sentence.get(i);
-//				postWord = sentence.get(i+1);
-//				if(isDoubleWord(word, postWord)){
-//					String stmp = sentence.get(i) + sentence.get(i+1);
-//                    sentence.set(i,stmp);
-//					sentence.remove(i+1);
-//				}
-//			}
-//			word = "";
-//	    };
 
 	public void adjust(TaggedSentence sentence) {
-		int size = sentence.size();
-		String word;
-		boolean hasTimeWord = false;
-
-		for (int i = size - 1; i >= 0; i--) {
-			word = "" + sentence.get(i).word;
-			if (isTimeWord(word)) {
-				hasTimeWord = true;
-			} else {
-				if (hasTimeWord) {
-					//if(isArabicNum(word) || isChineseNum(word)){
-					if (isArabicNum(word)) {
-						sentence.get(i).word += sentence.get(i + 1).word;
-						sentence.remove(i + 1);
-						sentence.get(i).tag = "t";
-					} else {
-						hasTimeWord = false;
-					}
-				}
-			}
-		}
-
-
-		size = sentence.size();
-		for (int i = 0; i < size; i++) {
-			word = sentence.get(i).word;
-			if (isHttpWord(word)) {
-				sentence.get(i).tag = "x";
-			}
-		}
-
-		size = sentence.size();
-		String preWord;
-		for (int i = 1; i < size; i++) {
-			preWord = sentence.get(i - 1).word;
-			word = sentence.get(i).word;
-			if (preWord.length() == 1 && preWord.charAt(0) == 64) {
-				if ((word.length() != 1) || (word.charAt(0) != 64)) {
-					sentence.get(i).tag = "np";
-				}
-			}
-		}
-
-		word = "";
+		processTimeWords(sentence);
+		processHttpWords(sentence);
+		processMailAddress(sentence);
 	}
 
 	public void adjustDouble(TaggedSentence sentence) {
-		int size = sentence.size();
-		String word;
-		boolean hasTimeWord = false;
-
-		for (int i = size - 1; i >= 0; i--) {
-			word = sentence.get(i).word;
-			if (isTimeWord(word)) {
-				hasTimeWord = true;
-			} else {
-				if (hasTimeWord) {
-					//if(isArabicNum(word) || isChineseNum(word)){
-					if (isArabicNum(word)) {
-						sentence.get(i).word += sentence.get(i + 1).word;
-						sentence.remove(i + 1);
-						sentence.get(i).tag = "t";
-					} else {
-						hasTimeWord = false;
-					}
-				}
-			}
-		}
-
-		size = sentence.size();
-		String postWord;
-		for (int i = size - 2; i >= 0; i--) {
-			word = sentence.get(i).word;
-			postWord = sentence.get(i + 1).word;
-			if (isDoubleWord(word, postWord)) {
-				sentence.get(i).word += sentence.get(i + 1).word;
-				sentence.remove(i + 1);
-			}
-		}
-
-		size = sentence.size();
-		for (int i = 0; i < size; i++) {
-			word = sentence.get(i).word;
-			if (isHttpWord(word)) {
-				sentence.get(i).tag = "x";
-			}
-		}
-
-		size = sentence.size();
-		String preWord;
-		for (int i = 1; i < size; i++) {
-			preWord = sentence.get(i - 1).word;
-			word = sentence.get(i).word;
-			if (preWord.length() == 1 && preWord.charAt(0) == 64) {
-				if ((word.length() != 1) || (word.charAt(0) != 64)) {
-					sentence.get(i).tag = "np";
-				}
-			}
-		}
-
-		word = "";
+		processTimeWords(sentence);
+		processDoubleWords(sentence);
+		processHttpWords(sentence);
+		processMailAddress(sentence);
 	}
 
+	private void processDoubleWords(TaggedSentence sentence) {
+		WordWithTag tagged, last = sentence.get(sentence.size() - 1);
+		for (int i = sentence.size() - 2; i >= 0; i--) {
+			tagged = sentence.get(i);
+			if (isDoubleWord(tagged.word, last.word)) {
+				tagged.word += last.word;
+				sentence.remove(i + 1);
+			}
+			last = tagged;
+		}
+	}
+
+	private void processTimeWords(TaggedSentence sentence) {
+		boolean hasTimeWord = false;
+		for (int i = sentence.size() - 1; i >= 0; i--) {
+			WordWithTag tagged = sentence.get(i);
+			if (isTimeWord(tagged.word)) hasTimeWord = true;
+			else if (hasTimeWord) {
+				if (isArabicNum(tagged.word)) {
+					tagged.word += sentence.remove(i + 1).word;
+					tagged.tag = "t";
+				} else hasTimeWord = false;
+			}
+		}
+	}
+
+	private void processHttpWords(TaggedSentence sentence) {
+		for (WordWithTag tagged : sentence)
+			if (this.isHttpWord(tagged.word)) tagged.tag = "x";
+	}
+
+	private void processMailAddress(TaggedSentence sentence) {
+		WordWithTag last = sentence.get(0), tagged;
+		for (int i = 1, size = sentence.size(); i < size; i++) {
+			tagged = sentence.get(i);
+			if ("@".equals(last.word) && !"@".equals(tagged.word)) tagged.tag = "np";
+			last = tagged;
+		}
+	}
 }
