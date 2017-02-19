3ea11c4baffe63bf576829d843f715bde67fa44d
std4453 <std4453@foxmail.com>
2017-02-19 23:48:00 +0800

    Cleanup Punctuation.java

--- src/main/java/org/thunlp/manage/Punctuation.java
+++ src/main/java/org/thunlp/manage/Punctuation.java
@@ -2,118 +2,44 @@ package org.thunlp.manage;
 
 import org.thunlp.base.Dat;
 import org.thunlp.base.TaggedSentence;
+import org.thunlp.base.WordWithTag;
 
 import java.io.IOException;
 import java.util.Vector;
 
 public class Punctuation {
-	Dat p_dat;
+	private Dat p_dat;
 
 	public Punctuation(String filename) throws IOException {
 		this.p_dat = new Dat(filename);
 	}
 
-	    /*
-		void adjust(TaggedSentence& sentence){
-	        if(!p_dat)return;
-	        for(int i = 0 ; i < sentence.size(); i ++){
-	            Word tmp = sentence[i].word;
-
-				if(p_dat->match(tmp) != -1){
-					sentence[i].tag = "w";
-				}
-	        }
-	    };
-	    */
-
-//	    public void adjust(SegmentedSentence sentence){
-//	        if(p_dat == null)return;
-//	        Vector<String> tmpVec = new Vector<String>();
-//	        for(int i = 0 ; i < sentence.size(); i ++){
-//	            String tmp = sentence.get(i);
-//	            if(p_dat.getInfo(tmp) >= 0) continue;
-//
-//	            //std::cout<<tmp<<std::endl;
-//
-//	            tmpVec.clear();
-//	            int j;
-//	            for(j = i + 1; j < sentence.size(); j ++){
-//	                tmp += sentence.get(j);
-//	                if(p_dat.getInfo(tmp) >= 0){
-//	                    break;
-//	                }
-//	                tmpVec.add(tmp);
-//	            }
-//	            int vecSize = (int)tmpVec.size();
-//
-//	            //std::cout<<vecSize<<std::endl;
-//	            for(int k = vecSize - 1; k >= 0; k--){
-//	                tmp = tmpVec.get(k);
-//	                //std::cout<<k<<":"<<tmp<<std::endl;
-//	                if(p_dat.match(tmp) != -1){
-//	                    //std::cout<<p_dat->match(tmp)<<std::endl;
-//	                    for(j = i + 1; j < i + k + 2; j ++){
-//	                    	String stmp = sentence.get(i) + sentence.get(j);
-//	                        sentence.set(i,stmp);
-//	                    }
-//	                    for(j = i + k + 1; j > i; j--){
-//	                        sentence.remove(j);
-//	                    }
-//	                    break;
-//	                }
-//	            }
-//	        }
-//	        tmpVec.clear();
-//	    };
-
 	public void adjust(TaggedSentence sentence) {
 		if (this.p_dat == null) return;
-		Vector<String> tmpVec = new Vector<>();
-		boolean findMulti = false;
+
+		Vector<String> tmp = new Vector<>();
 		for (int i = 0; i < sentence.size(); i++) {
-			String tmp = sentence.get(i).word;
-			if (this.p_dat.getInfo(tmp) >= 0) continue;
+			WordWithTag tagged = sentence.get(i);
+			StringBuilder sb = new StringBuilder(tagged.word);
+			if (this.p_dat.getInfo(sb.toString()) >= 0) continue;
 
-			//std::cout<<tmp<<std::endl;
-
-			tmpVec.clear();
-			int j;
-			for (j = i + 1; j < sentence.size(); j++) {
-				tmp += sentence.get(j).word;
-				if (this.p_dat.getInfo(tmp) >= 0) {
-					break;
-				}
-				tmpVec.add(tmp);
-			}
-			int vecSize = (int) tmpVec.size();
-
-			//std::cout<<vecSize<<std::endl;
-			findMulti = false;
-			for (int k = vecSize - 1; k >= 0; k--) {
-				tmp = tmpVec.get(k);
-				//std::cout<<k<<":"<<tmp<<std::endl;
-				if (this.p_dat.match(tmp) != -1) {
-					//std::cout<<p_dat->match(tmp)<<std::endl;
-					for (j = i + 1; j < i + k + 2; j++) {
-						sentence.get(i).word += sentence.get(j).word;
-					}
-					for (j = i + k + 1; j > i; j--) {
-						sentence.remove(j);
-					}
-					sentence.get(i).tag = "w";
-					findMulti = true;
-					break;
-				}
+			tmp.clear();
+			for (int j = i + 1; j < sentence.size(); j++) {
+				sb.append(sentence.get(j).word);
+				if (this.p_dat.getInfo(sb.toString()) >= 0) break;
+				tmp.add(sb.toString());
 			}
 
-			if (!findMulti) {
-				if (this.p_dat.match(sentence.get(i).word) != -1) {
-					sentence.get(i).tag = "w";
-				}
-			}
+			int k = tmp.size() - 1;
+			for (; k >= 0 && this.p_dat.match(tmp.get(k)) != -1; k--) ;
+			if (k >= 0) {
+				sb.setLength(0);
+				for (int j = i; j < i + k + 2; j++) sb.append(sentence.get(j).word);
+				tagged.word = sb.toString();
+				tagged.tag = "w";
 
+				for (int j = i + k + 1; j > i; j--) sentence.remove(j);
+			} else if (this.p_dat.match(tagged.word) != -1) tagged.tag = "w";
 		}
-		tmpVec.clear();
 	}
-
 }
