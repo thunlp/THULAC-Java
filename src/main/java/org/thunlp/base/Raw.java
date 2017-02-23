7d1588caf61395c87a8342a7365f2ece0985a433
std4453 <std4453@foxmail.com>
2017-02-23 23:47:19 +0800

    Remove Raw.java and SegmentedSentence.java
    Since they are merely direct aliases for some classes in the Java Standard Library and currently unused. There's clearly no need to keep them in the repo.

--- src/main/java/org/thunlp/base/Raw.java
+++
@@ -1,7 +0,0 @@
-package org.thunlp.base;
-
-import java.util.Vector;
-
-public class Raw extends Vector<Integer>{
-
-}
--- src/main/java/org/thunlp/base/SegmentedSentence.java
+++
@@ -1,7 +0,0 @@
-package org.thunlp.base;
-
-import java.util.Vector;
-
-public class SegmentedSentence extends Vector<String>{
-
-}
